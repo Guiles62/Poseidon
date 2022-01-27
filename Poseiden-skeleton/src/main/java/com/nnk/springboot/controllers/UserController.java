93c73ccd0c613d5484c8f0a252cb2bf6b3200dba
Guiles62 <https://github.com/Guiles62/P4.git>
2022-01-27 11:39:46 +0100

    add UserService

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.User;
-import com.nnk.springboot.repositories.UserRepository;
+import com.nnk.springboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -17,12 +17,11 @@ import javax.validation.Valid;
 @Controller
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @RequestMapping("/user/list")
-    public String home(Model model)
-    {
-        model.addAttribute("users", userRepository.findAll());
+    public String home(Model model) {
+        model.addAttribute("users", userService.getUserList());
         return "user/list";
     }
 
@@ -36,8 +35,8 @@ public class UserController {
         if (!result.hasErrors()) {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             user.setPassword(encoder.encode(user.getPassword()));
-            userRepository.save(user);
-            model.addAttribute("users", userRepository.findAll());
+            userService.addUser(user);
+            model.addAttribute("users", userService.getUserList());
             return "redirect:/user/list";
         }
         return "user/add";
@@ -45,7 +44,7 @@ public class UserController {
 
     @GetMapping("/user/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        User user = userService.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         user.setPassword("");
         model.addAttribute("user", user);
         return "user/update";
@@ -57,20 +56,17 @@ public class UserController {
         if (result.hasErrors()) {
             return "user/update";
         }
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        user.setPassword(encoder.encode(user.getPassword()));
-        user.setId(id);
-        userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
+        user = userService.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userService.addUser(user);
+        model.addAttribute("users", userService.getUserList());
         return "redirect:/user/list";
     }
 
     @GetMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        userRepository.delete(user);
-        model.addAttribute("users", userRepository.findAll());
+        User user = userService.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userService.delete(user);
+        model.addAttribute("users", userService.getUserList());
         return "redirect:/user/list";
     }
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserService.java
@@ -1,11 +1,42 @@
 package com.nnk.springboot.services;
 
 
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    @Autowired
+    UserRepository userRepository;
 
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    public User addUser (User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findById (int id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser (int id, User user) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void delete (User user) {
+        userRepository.delete(user);
+    }
 
 }
